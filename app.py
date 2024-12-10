edc2afdd91df7a28a18177d81433172f98ca56e8
abhisaranand <abhisar.muz@gmail.com>
2024-12-10 00:31:32 -0500

    made UI better

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, send_file
+from flask import Flask, request, render_template, send_file, url_for
 from werkzeug.utils import secure_filename
 import os
 from utils import generate_custom_qr_code
@@ -10,12 +10,15 @@ app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024  # 2 MB limit for uploads
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
 	os.makedirs(app.config['UPLOAD_FOLDER'])
 
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
+	qr_url = None
 	if request.method == 'POST':
 		link = request.form['link']
 		logo = request.files.get('logo')
-		output_path = os.path.join(app.config['UPLOAD_FOLDER'], 'generated_qr_code.png')
+		output_filename = 'generated_qr_code.png'
+		output_path = os.path.join(app.config['UPLOAD_FOLDER'], output_filename)
 
 		logo_path = None
 		if logo:
@@ -24,14 +27,27 @@ def home():
 
 		try:
 			generate_custom_qr_code(link, logo_path, output_path)
-			return send_file(output_path, as_attachment=True)
+			qr_url = url_for('static', filename=f'uploads/{output_filename}')
 		finally:
 			if logo_path and os.path.exists(logo_path):
 				os.remove(logo_path)
+
+	return render_template('index.html', qr_url=qr_url)
+
+@app.route('/download', methods=['GET'])
+def download():
+	output_filename = 'generated_qr_code.png'
+	output_path = os.path.join(app.config['UPLOAD_FOLDER'], output_filename)
+
+	if os.path.exists(output_path):
+		try:
+			return send_file(output_path, as_attachment=True)
+		finally:
+			# Ensure the QR code is deleted after being served
 			if os.path.exists(output_path):
 				os.remove(output_path)
-
-	return render_template('index.html')
+	else:
+		return "File not found", 404
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 10000))
