1d90305471d52e3d8e18001742699edca0416b59
abhisaranand <abhisar.muz@gmail.com>
2024-12-10 00:17:20 -0500

    automatically creates a folder if it doesn't exist, and also cleans up tmp files

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024  # 2 MB limit for uploads
 
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+	os.makedirs(app.config['UPLOAD_FOLDER'])
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
 	if request.method == 'POST':
@@ -14,15 +17,19 @@ def home():
 		logo = request.files.get('logo')
 		output_path = os.path.join(app.config['UPLOAD_FOLDER'], 'generated_qr_code.png')
 
+		logo_path = None
 		if logo:
 			logo_path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(logo.filename))
 			logo.save(logo_path)
-			generate_custom_qr_code(link, logo_path, output_path)
-			os.remove(logo_path)  # Clean up the uploaded logo
-		else:
-			generate_custom_qr_code(link, None, output_path)
 
-		return send_file(output_path, as_attachment=True)
+		try:
+			generate_custom_qr_code(link, logo_path, output_path)
+			return send_file(output_path, as_attachment=True)
+		finally:
+			if logo_path and os.path.exists(logo_path):
+				os.remove(logo_path)
+			if os.path.exists(output_path):
+				os.remove(output_path)
 
 	return render_template('index.html')
 
